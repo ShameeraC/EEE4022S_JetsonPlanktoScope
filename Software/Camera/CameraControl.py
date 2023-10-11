e9f95fa0435f7ab5f29f0ee9dcce6de6e2558df6
ShameeraC <s.cassim105@gmail.com>
2023-10-11 17:29:21 +0200

    repo restructuring

---
+++ Software/Camera/CameraControl.py
@@ -0,0 +1,8 @@
+### Camera Control
+
+# Imports
+import cv2
+import numpy as np
+from simple_camera import *
+
+show_camera()
---
+++ Software/Pump/pump_control.py
@@ -0,0 +1,84 @@
+# Pump control
+#imports
+import RPi.GPIO as GPIO
+import time
+import cv2
+import keyboard
+
+#define pins
+enable_pin = 33
+in1_pin = 35
+in2_pin = 36
+
+def conf():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(enable_pin, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(in1_pin, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(in2_pin, GPIO.OUT, initial=GPIO.LOW)
+
+def start():
+    GPIO.output(enable_pin, GPIO.HIGH)
+
+def stop():
+    GPIO.output(enable_pin, GPIO.LOW)
+    GPIO.output(in1_pin, GPIO.LOW)
+    GPIO.output(in2_pin, GPIO.LOW)
+
+
+def forward():
+    start()
+    GPIO.output(in1_pin, GPIO.HIGH)
+    GPIO.output(in2_pin, GPIO.LOW)
+
+def reverse():
+    start()
+    GPIO.output(in1_pin, GPIO.LOW)
+    GPIO.output(in2_pin, GPIO.HIGH)
+
+def main():
+    conf()
+    # forward()
+    # time.sleep(15)
+    # stop()
+    while True:
+        keyCode = cv2.waitKey(10) & 0xFF
+        if keyboard.is_pressed('q') or keyCode == 27:
+            print("closing...")
+            break
+        elif keyboard.is_pressed('f'):
+            print("forward...")
+            forward()
+            time.sleep(15)
+            stop()
+        elif keyboard.is_pressed('r'):
+            print("reverse...")
+            reverse()
+            time.sleep(15)
+            stop()
+        elif keyboard.is_pressed('x'):
+            print("disabled...")
+            stop()
+
+conf()
+main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
