7a0092780f89b57e2ffd5dbc2c626edef1cde91c
ShameeraC <s.cassim105@gmail.com>
2023-10-12 16:22:36 +0200

    added fluidics window (working) and updated to work on linux. Needs to be run as root for fluidics commands to work

--- Software/Dashboard/UI.py
+++ Software/Dashboard/UI.py
@@ -4,9 +4,16 @@ from tkinter import ttk
 from PIL import ImageTk, Image
 import math
 import UIFunctions as UIF
+import sys
+sys.path.insert(0, '/home/shameera/Desktop/EEE4022S_JetsonPlanktoScope/Software/Control')
+import simple_camera as cam
+import pump_control as pump
 
 def Camera():
-    print("TBC")
+    cam.show_camera()
+
+def Pump():
+    pump.pumpControl()
 
 # function to open a new window on a button click
 # based on https://www.geeksforgeeks.org/tkinter-application-to-switch-between-different-page-frames/
@@ -29,6 +36,28 @@ def openOpticsWindow():
     # button to start camera
     camButton = Button(newWindow, text="Start Camera", background='#3A7BB5', foreground='white', command= Camera)
     camButton.pack()
+
+# function to open a new window on a button click
+# based on https://www.geeksforgeeks.org/tkinter-application-to-switch-between-different-page-frames/
+def openFluidicsWindow():
+     
+    # Toplevel object which will 
+    # be treated as a new window
+    newWindow = Toplevel(win)
+ 
+    # sets the title of the
+    # Toplevel widget
+    newWindow.title("Fluidic Acquisition")
+ 
+    # sets the geometry of toplevel
+    newWindow.geometry("400x400")
+ 
+    # A Label widget to show in toplevel
+    Label(newWindow, text ="Fluidic Acquisition:\n1. q to quit\n2. f to pump forward for 15 seconds\n3. r to pump backwards for 15 seconds\n4. x to stop pump").pack()
+
+    # button to start camera
+    pumpButton = Button(newWindow, text="Start Pump", background='#3A7BB5', foreground='white', command= Pump)
+    pumpButton.pack()
 
 #Create an instance of Tkinter frame
 win = Tk()
@@ -53,7 +82,7 @@ button_height = math.floor(button_width / 2)
 
 # button objects
 button1 = Button(frame, text="Optic Configuration", background='#3A7BB5', foreground='white', width=button_width, height=button_height, command= openOpticsWindow)
-button2 = Button(frame, text="Fluidic Acquisition", background='#3A7BB5', foreground='white', width=button_width, height=button_height)
+button2 = Button(frame, text="Fluidic Acquisition", background='#3A7BB5', foreground='white', width=button_width, height=button_height, command=openFluidicsWindow)
 button3 = Button(frame, text="Segmentation", background='#3A7BB5', foreground='white', width=button_width, height=button_height)
 button4 = Button(frame, text="Gallery", background='#3A7BB5', foreground='white', width=button_width, height=button_height)
 
@@ -64,17 +93,17 @@ button3.grid(row = 1, column = 3, pady = 100, padx = 100, sticky="NSEW")
 button4.grid(row = 1, column = 4, pady = 100, padx = 100, sticky="NSEW")
 
 # logo images
-PSPath = "Software\Dashboard\planktoscope_lavender.png"
+PSPath = "planktoscope_lavender.png"
 img = Image.open(PSPath)
-imgResized = img.resize((button_width*5, button_height*10), Image.Resampling.LANCZOS)
+imgResized = img.resize((button_width*5, button_height*10), Image.LANCZOS)
 PSlogo = ImageTk.PhotoImage(imgResized)
 PSlogoImg = Label(frame, image=PSlogo, background='#CCCCCC')
 PSlogoImg.photo = PSlogoImg
 PSlogoImg.grid(row = 2, column = 1, pady = 300, sticky="NSEW")
 
-UCTPath = "Software\Dashboard\logo.png"
+UCTPath = "logo.png"
 imgUCT = Image.open(UCTPath)
-imgUCTResized = imgUCT.resize((button_width*5, button_height*10), Image.Resampling.LANCZOS)
+imgUCTResized = imgUCT.resize((button_width*5, button_height*10), Image.LANCZOS)
 UCTlogo = ImageTk.PhotoImage(imgUCTResized)
 UCTlogoImg = Label(frame, image=UCTlogo, background='#CCCCCC')
 UCTlogoImg.photo = UCTlogoImg
